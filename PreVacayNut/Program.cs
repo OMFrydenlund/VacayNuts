18947c77ef99a13d8dd4002bcfe1273f304a05a4
Miguel <migfrydenlund@gmail.com>
2025-06-02 11:27:06 +0200

    Add project files.

---
+++ PreVacayNut/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+while (true)
+{
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    switch (num)
+    {
+        case > 0:
+            Console.WriteLine(1);
+            break;
+        case 0:
+            Console.WriteLine(num);
+            break;
+        case < 0:
+            Console.WriteLine(-1);
+            break;
+        default:
+            Console.WriteLine("Not valid.");
+            break;
+    }
+}
---
+++ PreVacayNuttier/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int maxCount = 101;
+
+for (int count = 0; count < maxCount; count++)
+{
+    Console.WriteLine(OfWhatEquivalent(count));
+}
+
+
+string OfWhatEquivalent(int num)
+{
+    string textToReturn = "";
+
+    if(num % 3 == 0 || num.ToString().Contains("3"))
+    {
+        textToReturn += "Fizz";
+    }
+    if (num % 5 == 0 || num.ToString().Contains("5"))
+    {
+        textToReturn += "Buzz";
+    }
+
+    if(textToReturn == "")
+    {
+        textToReturn = num.ToString();
+    }
+
+    return textToReturn;
+}
